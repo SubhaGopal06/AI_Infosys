1844a89ceef0621e165b21b05294be9a4cf95a6e
SubhaGopal06 <gksubha06@gmail.com>
2025-01-20 20:39:48 +0530

    Updated main.py

--- App/main.py
+++ App/main.py
@@ -1,1 +1,17 @@
+from typing import Union
 
+from fastapi import FastAPI #type: ignore
+
+app=FastAPI()
+
+@app.get("/")
+async def root():
+    return {"API endpoints" : "Sentiment, Responses"}
+
+@app.get("/get_sentiment")
+async def read_root():
+    return {"sentiment": "Positive"}
+
+@app.get("/get_response")
+async def read_item(item_id: int, q: Union[str, None]=None):
+    return {"response": "Your query is resolved"}
