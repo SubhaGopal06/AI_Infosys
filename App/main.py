6eb213b184f05ab8a3de9c5e889dc03d196c4aa9
Subha_Gopal <141430568+SubhaGopal06@users.noreply.github.com>
2025-01-20 20:42:52 +0530

    Update main.py

--- App/main.py
+++ App/main.py
@@ -15,3 +15,10 @@ async def read_root():
 @app.get("/get_response")
 async def read_item(item_id: int, q: Union[str, None]=None):
     return {"response": "Your query is resolved"}
+    
+# INFO:     Will watch for changes in these directories: ['C:\\Users\\G subhashini\\OneDrive\\Certificates.subha\\Projects\\Infosys-intern-5.0\\AI_infosys\\App']
+# INFO:     Uvicorn running on http://127.0.0.1:8000 (Press CTRL+C to quit)
+# INFO:     Started reloader process [50328] using WatchFiles
+# INFO:     Started server process [49124]
+# INFO:     Waiting for application startup.
+# INFO:     Application startup complete.
